d62af8e2a4917a8c36fb899fd7f03896a3343da2
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2021-11-17 17:12:41 +0530

    removed unwanted code 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 
-import { darkScrollbar, Grid } from '@mui/material';
 import './App.css';
-import ChatMenu from './App/ChatMenu';
-import Conversation from './App/Conversation';
-import ProfileMenu from './App/ProfileMenu';
-import SideBar from './App/SideBar';
 import {createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './App/login';
 import Home from './App/Home';
-import React, { useEffect } from 'react'
-import db from './FirebaseComp';
+import React from 'react'
 import People from './App/People';
 import Inbox from './App/Inbox';
 import Stats from './App/Stats';
-
-
 const theme = createTheme();
 
 function App() {
@@ -28,26 +20,22 @@ function App() {
   return (
         <ThemeProvider theme={theme}>
           <div className="App"
-          //  style={{display:'flex', alignItems:'center'}} 
            >
               { !loginData ? (
                 <Login sendLoginData={handleLoginData}/>
               ) : (
               <Router>
               <Routes>
-                {/* <Route path="/login" element={}/> */}
                 <Route path="/" element={ <Home loginData={loginData} />}/>
                 <Route path="/chat" element={ <Inbox loginData={loginData} />}/>
                 <Route path="/chat/:conversationId" element={ <Inbox loginData={loginData} />}/>
                 <Route path="/people" element={ <People loginData={loginData}/>}/>
                 <Route path="/stats" element={ <Stats loginData={loginData} />}/>
-                {/* <Route path="/login" element={ <Login sendLoginData={handleLoginData}/>}/> */}
               </Routes>
              </Router>
               )}
           </div>
         </ThemeProvider>
-
   );
 }
 
